fd22d03ec141064bc665f3f3b6c90a9c2c7a902d
Mohammed Junaid <139953853+MohammedJunaid1@users.noreply.github.com>
2025-02-08 21:09:34 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,7 +7,7 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const backendUrl = "https://prescripto-full-stack-backend-j5pr.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
 
